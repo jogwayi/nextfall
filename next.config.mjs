1be6f4294409fdb79df348ee1746a4e4ce514eac
Josephat <jogwayi@users.noreply.github.com>
2025-05-22 13:52:33 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    experimental: {
+        serverComponentsExternalPackages: ["@node-rs/argon2"],
+        serverActions: {
+            bodySizeLimit: '4mb',
+        }
+    },
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: "images.unsplash.com" },
+            { protocol: 'https', hostname: "crm.trpeglobal.com" },
+            { protocol: 'https', hostname: "trpe.ae" },
+            { protocol: 'https', hostname: "files.edgestore.dev" },
+            { protocol: 'https', hostname: "trpe-ae.s3.me-central-1.amazonaws.com" },
+            { protocol: 'https', hostname: "assets.aceternity.com" }
+        ],
+    },
+    // Removing redirects from next.config.mjs, now handled in middleware.ts
+    // This prevents overlapping redirect definitions
 };
 
-export default nextConfig;
+// Add this for bundle analysis
+import withBundleAnalyzerFactory from '@next/bundle-analyzer';
+const withBundleAnalyzer = withBundleAnalyzerFactory({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default withBundleAnalyzer(nextConfig);
